e1816450696012d6ed6b5373ac6c7b1e6db00661
Carlos Delgado <sdkcarlos@outlook.com>
2021-08-24 20:02:09 -0500

    Update iOS Plugin

--- ios/Plugin/CapacitorNativeFilePickerPlugin.m
+++ ios/Plugin/CapacitorNativeFilePickerPlugin.m
@@ -5,4 +5,6 @@
 // each method the plugin supports using the CAP_PLUGIN_METHOD macro.
 CAP_PLUGIN(CapacitorNativeFilePickerPlugin, "CapacitorNativeFilePicker",
            CAP_PLUGIN_METHOD(echo, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(launchFolderPicker, CAPPluginReturnPromise);
+           CAP_PLUGIN_METHOD(launchFilePicker, CAPPluginReturnPromise);
 )
--- ios/Plugin/CapacitorNativeFilePickerPlugin.swift
+++ ios/Plugin/CapacitorNativeFilePickerPlugin.swift
@@ -1,18 +1,88 @@
 import Foundation
 import Capacitor
+import MobileCoreServices
 
 /**
  * Please read the Capacitor iOS Plugin Development Guide
  * here: https://capacitorjs.com/docs/plugins/ios
  */
 @objc(CapacitorNativeFilePickerPlugin)
-public class CapacitorNativeFilePickerPlugin: CAPPlugin {
-    private let implementation = CapacitorNativeFilePicker()
-
-    @objc func echo(_ call: CAPPluginCall) {
-        let value = call.getString("value") ?? ""
-        call.resolve([
-            "value": implementation.echo(value)
+public class CapacitorNativeFilePickerPlugin: CAPPlugin, UIDocumentPickerDelegate {
+    private var pickerType: String = ""
+    public var _call: CAPPluginCall? = nil
+    
+    @objc func launchFilePicker(_ call: CAPPluginCall){
+        let allowMultipleSelection = ((call.getInt("limit") ?? -1) == -1) ? true: false
+        
+        let this = self
+        
+        self._call = call
+        self.pickerType = "files"
+         
+        DispatchQueue.main.async { [weak self] in
+            let documentPicker = UIDocumentPickerViewController(
+                documentTypes: [String(kUTTypeItem)],
+                in: UIDocumentPickerMode.open
+            )
+            
+            documentPicker.allowsMultipleSelection = allowMultipleSelection
+            
+            if #available(iOS 13.0, *) {
+                documentPicker.shouldShowFileExtensions = true
+            }
+            
+            documentPicker.delegate = this
+            documentPicker.modalPresentationStyle = UIModalPresentationStyle.fullScreen
+            
+            self?.bridge?.viewController?.present(
+                documentPicker,
+                animated: true,
+                completion: nil
+            )
+        }
+    }
+    
+    @objc func launchFolderPicker(_ call: CAPPluginCall){
+        let this = self
+        self._call = call
+        
+        self.pickerType = "folders"
+        
+        DispatchQueue.main.async { [weak self] in
+            let documentPicker = UIDocumentPickerViewController(
+                documentTypes: [String(kUTTypeFolder)],
+                in: UIDocumentPickerMode.open
+            )
+            
+            documentPicker.allowsMultipleSelection = true
+            documentPicker.delegate = this
+            documentPicker.modalPresentationStyle = UIModalPresentationStyle.fullScreen
+            
+            self?.bridge?.viewController?.present(
+                documentPicker,
+                animated: true,
+                completion: nil
+            )
+        }
+    }
+    
+    /// This method responds when the user selects the file(s) or folder(s).
+    /// If the user selects something, the Capacitor plugin will return the array with the defined key to the view.
+    /// - Parameters:
+    ///   - controller: <#controller description#>
+    ///   - urls: urls contains an array of URL objects with the selected elements (files or folders).
+    public func documentPicker(
+        _ controller: UIDocumentPickerViewController,
+        didPickDocumentsAt urls: [URL]
+    ){
+        var items: [String] = []
+        
+        for url in urls {
+            items.append(url.absoluteString)
+        }
+        
+        self._call?.resolve([
+            self.pickerType: items
         ])
     }
 }
